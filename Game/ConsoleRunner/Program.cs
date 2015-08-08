b2ae92aabf8d5a20d4e40c21eb2b72d6125e673a
Nick Burlakov <burlakov.nick@gmail.com>
2015-08-08 15:20:56 +0500

    Балуюсь с ConsoleRunner'ом

--- Game/ConsoleRunner/Program.cs
+++ Game/ConsoleRunner/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using ICFPC2015.GameLogic.Logic;
 
 namespace ICFPC2015.ConsoleRunner
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            var game = new GameBuilder().Build(@"Problems\problem_0.json")[0];
+
+            var w = "ei!";
+            var commands = new[] {Command.MoveEast, Command.MoveSouthWest, Command.MoveWest};
+            for (int i = 0;; i ++)
+            {
+                Console.Write(w[i % 3]);
+                var command = commands[i % 3];
+                var result = game.TryMakeStep(command);
+                game = result.Game;
+                if (result.Result == StepResult.GameOver)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(game.Score);
         }
     }
 }
---
+++ Game/Game/Logic/Output/Output.cs
@@ -0,0 +1,7 @@
+﻿namespace ICFPC2015.GameLogic.Logic.Output
+{
+    public class Output
+    {
+        public int problemId { get; set; }
+    }
+}
