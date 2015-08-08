bebc8194722474567363ca24da6797668d77f18b
olgasob <olgasob@skbkontur.ru>
2015-08-08 14:30:58 +0500

    Проект для игрателя

---
+++ Game/Player/Implementation/ReachableStatesGetter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ICFPC2015.GameLogic.Logic;
+
+namespace ICFPC2015.Player.Implementation
+{
+    public struct ReachableStatesGetter
+    {
+        public GameUnit[] Get(Board board, GameUnit unit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
